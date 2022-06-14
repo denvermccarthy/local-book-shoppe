1d03514c14179c04971588fda40fd408dd0ad247
Denver McCarthy <dmichaelspdx@gmail.com>
2022-06-14 13:50:34 -0700

    added a get all function to book model and a data file for book data

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -1,5 +1,6 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
+const books = require('../data/books');
 const request = require('supertest');
 const app = require('../lib/app');
 
@@ -10,7 +11,7 @@ describe('backend-express-template routes', () => {
   test('/books should render an array of book objects', async () => {
     const req = await request(app).get('/books');
 
-    expect(req.body.length).toEqual(5);
+    expect(req.body).toEqual(books);
   });
   afterAll(() => {
     pool.end();
---
+++ data/books.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    id: '1',
+    title: 'Harry Potter and the Philosophers Stone',
+    released: 1997,
+  },
+  { id: '2', title: 'The Hobbit', released: 1937 },
+  { id: '3', title: 'Charlottes Web', released: 1952 },
+  { id: '4', title: 'Goodnight Moon', released: 1947 },
+  {
+    id: '5',
+    title: 'The Hitchhikers Guide to the Galaxy',
+    released: 1979,
+  },
+];
